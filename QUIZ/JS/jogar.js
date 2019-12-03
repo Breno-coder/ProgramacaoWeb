07f026705b14db62dff8b05ffe0f4a97f5d88148
Breno-coder <breno098526049@gmail.com>
2019-12-03 14:50:51 -0300

    Implementação da mecanica de jogo.

--- QUIZ/JS/jogar.js
+++ QUIZ/JS/jogar.js
@@ -1,7 +1,8 @@
 let perguntar = document.getElementById("pergunta");
 let prox  = document.getElementById("proximo");
-let i=-1, k=0,certas=0, erradas=0;
-let op = [document.getElementById("op1"), document.getElementById("op2"), document.getElementById("op3"), document.getElementById("op4")];
+let i=-1, k=0,certas=0, erradas=0, contadordevezes = 0;;
+
+let op = [document.getElementById("op1"), document.getElementById("op2"), document.getElementById("op3"), document.getElementById("op4"), document.getElementById("op5")];
 
 let quest = [
     'O que significa a sigla DOM?',
@@ -25,20 +26,123 @@ let btnResposta =   [["Document Object Model","Document Object Manipulate","Docu
                     ["04/06/1969","17/01/1970","28/12/1969","30/08/1970"],
                     ["Steve Wozniak","Bjarne Stroustrup","Ken Thompson","Tim Berners-Lee"],
                     ["Steve Jobs","Ada Lovelace","Rob Pike","Donald Knuth"],
-                    ["Yukihiro Matsumoto","Grace Hopper","Grace Hopper","Roberto Ierusalimschy"]];
+                    ["Yukihiro Matsumoto","Grace Hopper","Donald Knuth","Roberto Ierusalimschy"]];
 
-let btnRespostaCerta = [0,1,0,2,1,3,2,3,1,0];
+let tempogasto = [];
 
 prox.addEventListener("click", function(){
 i++;
 });
-if (i<quest.length) {
-   prox.addEventListener("click",function alterar(){ perguntar.innerHTML = quest[i]});
-   prox.addEventListener("click",function alterbtn(){
-       /*op[i].innerHTML = btnResposta[i][1];
-       op[i+1].innerHTML = btnResposta[i][1];
-       op[i+2].innerHTML = btnResposta[i][1];
-       op[i+3].innerHTML = btnResposta[i][1];*/
-   });
-}
+prox.addEventListener("click", function alterar(evento){ perguntar.innerHTML = quest[i]});
+prox.addEventListener("click",function alterbtn(){
+    if (i < 11) {
+        if (i == 10) {
+            perguntar.innerHTML = `Respostas certas: ${certas} | Respostas erradas: ${erradas}`;
+            op[k].innerHTML = `1º - ${tempogasto[0]}.   | 2º - ${tempogasto[1]}. | 3º - ${tempogasto[2]}.`;
+            op[k+1].innerHTML = `4º - ${tempogasto[3]}. | 5º - ${tempogasto[4]}. |6º - ${tempogasto[5]}.`;
+            op[k+2].innerHTML = `7º - ${tempogasto[6]}. | 8º - ${tempogasto[7]}. |`;
+            op[k+3].innerHTML = `9º - ${tempogasto[8]}. | 10º - ${tempogasto[9]}.|`;
+            op[k+4].remove();
+        }
+        op[k].innerHTML = btnResposta[i][k];
+        op[k+1].innerHTML = btnResposta[i][k+1];
+        op[k+2].innerHTML = btnResposta[i][k+2];
+        op[k+3].innerHTML = btnResposta[i][k+3];
 
+        validajogo();
+        
+        if (i < 10) {
+            setInterval(() => {
+            op[k+4].innerHTML = "Tempo Gasto : "+ min + "m"+" e "+ sec +"s"; 
+            }, 10);
+        }
+        tempogasto[i] = min +"m"+" : "+ sec + " s";
+        min=0;
+        sec=0;
+    }
+    else
+    {
+    window.location.href = "introducao.html"
+    }
+});
+
+let sec = 1, min=0;
+let cronometro = setInterval(() => {
+    if (sec == 60) {
+        min++;
+        sec=0;
+    }
+        sec = sec + 1;
+}, 1000);
+
+function validajogo(){
+    contadordevezes = 0;
+    op[0].addEventListener("click", function() {
+        if (contadordevezes == 0 && i<10) {
+            if (i==0 || i==2 || i==9) {
+                contadordevezes++;
+                return certas++;
+            }
+            else
+            {
+                contadordevezes++;
+                return erradas++;
+            }
+        }
+        else{
+            contadordevezes++;
+        }
+    });
+
+    op[1].addEventListener("click", function() {
+        if (contadordevezes == 0) {
+            if (i==1 || i==4 || i==8) {
+                contadordevezes++;
+                return certas++;
+            }
+            else
+            {
+                contadordevezes++;
+                return erradas++;
+            }
+        }
+        else{
+            contadordevezes++;
+        }
+    });
+
+    op[2].addEventListener("click", function() {
+        if (contadordevezes == 0) {
+            if (i==3 || i==6) {
+                contadordevezes++;
+                return certas++;
+            }
+            else
+            {
+                contadordevezes++;
+                return erradas++;
+            }
+        }
+        else{
+            contadordevezes++;
+        }
+
+    });
+
+    op[3].addEventListener("click",function() {
+        if (contadordevezes == 0) {
+            if (i==5 || i==7) {
+                contadordevezes++;
+                return certas++;
+            }
+            else
+            {
+                contadordevezes++;
+                return erradas++;
+            }
+        }
+        else{
+            contadordevezes++;
+        }
+    });
+}
--- QUIZ/JS/login.js
+++
@@ -1,8 +0,0 @@
-let btn = document.getElementById("entrar");
-let senha = document.getElementById("senha");
-let usernase = document.getElementById("username");
-btn.addEventListener("mousedown", function(){
-if (senha.value == "admin" && username.value == "admin") {
-    window.location.href = "index.html";
-}    
-});
